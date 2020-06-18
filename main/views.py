36e46fbc67a6dd14a890e4031da1e90ab141e003
Ivanov Fedor <ivnfedor@gmail.com>
2020-06-18 20:17:26 +0300

    Added pagination and ordering

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from rest_framework import filters
 from rest_framework.generics import ListCreateAPIView
 
 from . import models
@@ -7,3 +8,5 @@ from . import serializers
 class ReviewAPIView(ListCreateAPIView):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['id', 'created_at']
