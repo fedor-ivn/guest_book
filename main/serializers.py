6354205c0de831ae28ec851d2aaa9db821ebb16e
Ivanov Fedor <ivnfedor@gmail.com>
2020-06-18 17:27:26 +0300

    Added validation and review list rendering

--- main/serializers.py
+++ main/serializers.py
@@ -4,6 +4,8 @@ from . import models
 
 
 class ReviewSerializer(serializers.ModelSerializer):
+    created_at_humanized = serializers.CharField()
+
     class Meta:
         model = models.Review
         fields = '__all__'
